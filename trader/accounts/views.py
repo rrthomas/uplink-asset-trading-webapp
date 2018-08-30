d5d784fd69a1c8615baddc21d76cc07e11f08c26
David Salgado <david@digitalronin.com>
2018-08-30 17:26:04 +0100

    List the non-validator uplink accounts
    This is not the best way to connect to uplink - it
    should be possible to instantiate the RPC object
    once in the app and have it available in all the 
    views. Also, I should be using templates instead
    of hand-rolling the HTML, but it's a start.

--- trader/accounts/views.py
+++ trader/accounts/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
+from uplink import *
 
 # Create your views here.
 
+def is_validator(account):
+    if 'validator' in account.metadata:
+        return account.metadata['validator'] == 'true'
+
+
 def index(request):
-    return HttpResponse("<h2>Hello world, accounts/index</h2>")
+    rpc = UplinkJsonRpc(host="localhost", port=8545, tls=False)
+    accounts = rpc.uplink_accounts()
+
+    rtn = "<h2>Accounts</h2>\n"
+
+    for acct in accounts:
+        if not is_validator(acct):
+            rtn += "<p>" + acct.address + "</p>"
+
+    return HttpResponse(rtn)
